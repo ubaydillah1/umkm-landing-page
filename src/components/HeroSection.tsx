6f097691515a67fdc0d4b1f2fe3688e8266b83ca
Ahmad Dani <putraahmaddani833@gmail.com>
2025-11-02 21:34:45 +0700

    feat: add simple animation in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,28 +1,68 @@
+import { motion } from "motion/react";
 import { Button } from "./ui/button";
+import type { Variants } from "motion/react";
 
-const HeroSection = () => {
+export default function HeroSection() {
+  const container: Variants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delay: 5,
+        staggerChildren: 0.2,
+        delayChildren: 0.3,
+      },
+    },
+  };
+
+  const item: Variants = {
+    hidden: { opacity: 0, y: 40 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { delay: 5, duration: 0.8, ease: [0.25, 0.1, 0.25, 1] },
+    },
+  };
+
   return (
-    <section className="sm:pt-[120px] container space-y-7 sm:space-y-10 pt-[100px] pb-10">
-      <p className="text-center text-primary flex justify-center gap-2.5">
-        <div className="animate-shake-slow">👋</div>{" "}
+    <motion.section
+      className="sm:pt-[120px] container space-y-7 sm:space-y-10 pt-[100px] pb-10"
+      variants={container}
+      initial="hidden"
+      animate="show"
+    >
+      <motion.p
+        variants={item}
+        className="text-center text-primary flex justify-center gap-2.5"
+      >
+        <div className="animate-shake-slow">👋</div>
         <span className="text-sm sm:text-lg">Hi, Kita dari kelompok ...</span>
-      </p>
-      <h1 className="text-center text-[32px] sm:text-[100px] font-recoleta mx-auto sm:leading-[100px] ">
+      </motion.p>
+
+      <motion.h1
+        variants={item}
+        className="text-center text-[32px] sm:text-[100px] font-recoleta mx-auto sm:leading-[100px]"
+      >
         Menggali Potensi Lokal, Membawa UMKM ke Layar Global.
-      </h1>
-      <p className="text-center sm:max-w-[400px] mx-auto text-[12px] max-w-[80%] sm:text-sm opacity-70">
+      </motion.h1>
+
+      <motion.p
+        variants={item}
+        className="text-center sm:max-w-[400px] mx-auto text-[12px] max-w-[80%] sm:text-sm opacity-70"
+      >
         Kami menjembatani antara kreasi lokal dan pasar digital, menciptakan
         branding yang kuat, dan membangun ekosistem bisnis yang berkelanjutan.
-      </p>
+      </motion.p>
 
-      <div className="mx-auto flex gap-4 sm:max-w-[400px] max-w-[300px]">
+      <motion.div
+        variants={item}
+        className="mx-auto flex gap-4 sm:max-w-[400px] max-w-[300px]"
+      >
         <Button className="flex-1">Strategi Kami</Button>
-        <Button className="flex-1" variant={"secondary"}>
+        <Button className="flex-1" variant="secondary">
           List UMKM
         </Button>
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   );
-};
-
-export default HeroSection;
+}
